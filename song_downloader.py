8f007a95de4eb5a555241d0b7cd021114a498cfd
SrikarMannepalli <37926341+SrikarMannepalli@users.noreply.github.com>
2020-07-16 22:38:30 +0530

    Update song_downloader.py

--- song_downloader.py
+++ song_downloader.py
@@ -11,18 +11,19 @@ def give_options(video_data):
     tabledata = [['S No.', 'Track Title', 'Track length']]
     content_check = video_data["contents"]["twoColumnSearchResultsRenderer"]["primaryContents"][
         "sectionListRenderer"]["contents"][0]
+
+    # bypass ads
     ind = 0
     if 'promotedSparklesTextSearchRenderer' in str(content_check):
         ind = 1
-    # print(video_data["contents"]["twoColumnSearchResultsRenderer"]["primaryContents"]["sectionListRenderer"]["contents"][1])
+
     content = video_data["contents"]["twoColumnSearchResultsRenderer"]["primaryContents"][
         "sectionListRenderer"]["contents"][ind]["itemSectionRenderer"]["contents"]
-
+    
     ids = []
-    # print(video_data["contents"]["twoColumnSearchResultsRenderer"]["primaryContents"]["sectionListRenderer"]["contents"][0])
     for i in range(0, min(len(content), 10)):
+        # try except to get out of did you mean renders and encountering playlists and stuff
         try:
-        # print(i)
             title = content[i]["videoRenderer"]["title"]["runs"][0]["text"]
             length = content[i]["videoRenderer"]["lengthText"]["simpleText"]
             video_id = content[i]["videoRenderer"]["videoId"]
@@ -45,7 +46,6 @@ for bd in body:
     y = str(bd)
     if 'window["ytInitialData"]' not in y:
         continue
-    # print(y)
     ind1 = y.rfind('"responseContext')
     ind2 = y.rfind('window["ytInitialPlayerResponse"]')
     y = y[ind1 - 1:ind2 - 1].strip()[:-1]
